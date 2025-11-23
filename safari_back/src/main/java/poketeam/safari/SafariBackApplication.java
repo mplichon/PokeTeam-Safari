0f5cfbae96cd8f1e8505950418760aad3e3b8776
mplichon <plichon.matteo@gmail.com>
2025-11-23 14:34:05 +0100

    ajout de spring boot

---
+++ safari_back/src/main/java/poketeam/safari/SafariBackApplication.java
@@ -0,0 +1,13 @@
+package poketeam.safari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafariBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafariBackApplication.class, args);
+	}
+
+}
---
+++ safari_back/src/test/java/poketeam/safari/SafariBackApplicationTests.java
@@ -0,0 +1,13 @@
+package poketeam.safari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafariBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
