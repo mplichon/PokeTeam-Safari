d4ab7b778962d58b0abc48902d3cd525938fa7b1
mplichon <plichon.matteo@gmail.com>
2025-11-28 18:27:30 +0100

    DAO et Service pour Compte

--- safari_back/src/main/java/poketeam/safari/dao/IDAOCompte.java
+++ safari_back/src/main/java/poketeam/safari/dao/IDAOCompte.java
@@ -1,9 +1,24 @@
 package poketeam.safari.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import poketeam.safari.model.Admin;
 import poketeam.safari.model.Compte;
+import poketeam.safari.model.Joueur;
 
 public interface IDAOCompte extends JpaRepository<Compte, Integer> {
 
+    @Query("FROM Joueur")
+    public List<Joueur> findAllJoueurs();
+
+    @Query("FROM Admin")
+    public List<Admin> findAllAdmins();
+
+    public Compte findByLoginAndPassword(String login, String password);
+
+    public Optional<Compte> findByLogin(String login);
 }
