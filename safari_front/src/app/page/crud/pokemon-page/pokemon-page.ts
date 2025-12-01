29430b1547b6f417abe789a6ce67a807419eb8b8
mplichon <plichon.matteo@gmail.com>
2025-12-01 15:39:03 +0100

    ajout de la page pokemon crud

---
+++ safari_front/src/app/page/crud/pokemon-page/pokemon-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-page',
+  imports: [],
+  templateUrl: './pokemon-page.html',
+  styleUrl: './pokemon-page.css',
+})
+export class PokemonPage {
+
+}
