72716fd999ff4238d3aab013c62b4781944858ca
Alyssa <tasafut013@gmail.com>
2025-12-02 15:24:58 +0100

    Admin un peu 2 tout
    Service, page et dto

--- safari_front/src/app/app.routes.ts
+++ safari_front/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { LoginPage } from './page/login-page/login-page';
 import { GamePage } from './page/game-page/game-page';
 import { TestPage } from './page/test-page/test-page';
 import { PokemonPage } from './page/crud/pokemon-page/pokemon-page';
+import { AdminPage } from './page/crud/admin-page/admin-page';
 
 export const routes: Routes = [
     {path: '', component: LoginPage},
     {path: 'login', component: LoginPage},
     {path: 'game', component: GamePage},
     {path: 'test', component: TestPage},
-    {path: 'pokemon', component: PokemonPage}
+    {path: 'pokemon', component: PokemonPage},
+    {path: 'admin', component: AdminPage}
+    
 ];
