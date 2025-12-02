e6a13d4c0db06aab8c7f72135c2922cae7c48180
Martin_G <mgabut.pro@gmail.com>
2025-12-02 09:41:36 +0100

    fix interceptor

--- safari_front/src/app/interceptor/api-url-interceptor-interceptor.ts
+++ safari_front/src/app/interceptor/api-url-interceptor-interceptor.ts
@@ -2,6 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 export const apiUrlInterceptor: HttpInterceptorFn = (req, next) => {
+  
+  if (req.url.startsWith('http')) {
+    return next(req); 
+}
+  
   const apiReq = req.clone({
     url: environment.apiUrl + req.url
   });
