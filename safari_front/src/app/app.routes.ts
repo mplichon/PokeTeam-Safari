42988cf19fbb21a89b638d7624afc50ce0af6883
Martin_G <mgabut.pro@gmail.com>
2025-12-02 11:45:06 +0100

    routes

--- safari_front/src/app/app.routes.ts
+++ safari_front/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './page/login-page/login-page';
 import { GamePage } from './page/game-page/game-page';
 import { TestPage } from './page/test-page/test-page';
+import { PokemonPage } from './page/crud/pokemon-page/pokemon-page';
+import { authGuard } from './guard/auth-guard';
 
 export const routes: Routes = [
     {path: '', component: LoginPage, canActivate: [ authGuard ]},
     {path: 'login', component: LoginPage},
-    {path: 'pokemon', component: PokemonPage}
-    {path: 'test', component: TestPage},
-    {path: 'game', component: GamePage},
+    {path: 'pokemon', component: PokemonPage},
+    {path: 'test', component: TestPage, canActivate: [ authGuard ]},
+    {path: 'game', component: GamePage, canActivate: [ authGuard ]},
 ];
