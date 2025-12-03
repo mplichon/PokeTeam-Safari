603c451f7f0757a6b81a0f86362a24098ff42198
Alyssa <tasafut013@gmail.com>
2025-12-03 15:42:43 +0100

    help me bouton random
    help me bouton random

--- safari_front/src/app/component/carte/carte.ts
+++ safari_front/src/app/component/carte/carte.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { BoutonComponent } from "./bouton/bouton";
+import { ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: 'app-carte',
-  imports: [],
+  standalone: true,
+  imports: [BoutonComponent],
   templateUrl: './carte.html',
-  styleUrl: './carte.css',
+  styleUrls: ['./carte.css'],
 })
 export class Carte {
 
+ @Output() toggleFromRandom = new EventEmitter<void>(); // 💥 NOUVEAU
 
+  @ViewChild('zone', { static: true }) zone!: ElementRef;
+  zoneRef!: ElementRef;
 
+  ngAfterViewInit() {
+    this.zoneRef = this.zone;
+  }
+
+  onRandomClick() {
+    this.toggleFromRandom.emit();  // 💥 on prévient le parent (GamePage)
+  }
 }
