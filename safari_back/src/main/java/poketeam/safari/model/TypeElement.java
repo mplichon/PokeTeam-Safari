bd3a43876ba84bd18ea26983947340cc5b85975a
mplichon <plichon.matteo@gmail.com>
2025-12-03 14:08:18 +0100

    Ajout de l'attribut couleur dans l'enum TypeElement

--- safari_back/src/main/java/poketeam/safari/model/TypeElement.java
+++ safari_back/src/main/java/poketeam/safari/model/TypeElement.java
@@ -1,31 +1,33 @@
 package poketeam.safari.model;
 
 public enum TypeElement {
-	ACIER("Acier"),
-	COMBAT("Combat"),
-	DRAGON("Dragon"),
-	EAU("Eau"),
-	ELECTRIK("Électrik"),
-	FEE("Fée"),
-	FEU("Feu"),
-	GLACE("Glace"),
-	INSECTE("Insecte"),
-	NORMAL("Normal"),
-	PLANTE("Plante"),
-	POISON("Poison"),
-	PSY("Psy"),
-	ROCHE("Roche"),
-	SOL("Sol"),
-	SPECTRE("Spectre"),
-	TENEBRES("Ténèbres"),
-	VOL("Vol");
+	ACIER("Acier", "#60A2B9"),
+	COMBAT("Combat", "#FF8100"),
+	DRAGON("Dragon", "#4F60E2"),
+	EAU("Eau", "#2481EF"),
+	ELECTRIK("Électrik", "#FAC100"),
+	FEE("Fée", "#EF70EF"),
+	FEU("Feu", "#E72324"),
+	GLACE("Glace", "#3DD9FF"),
+	INSECTE("Insecte", "#92A212"),
+	NORMAL("Normal", "#A0A2A0"),
+	PLANTE("Plante", "#3DA224"),
+	POISON("Poison", "#923FCC"),
+	PSY("Psy", "#EF3F7A"),
+	ROCHE("Roche", "#B0AA82"),
+	SOL("Sol", "#B0AA82"),
+	SPECTRE("Spectre", "#703F70"),
+	TENEBRES("Ténèbres", "#4F3F3D"),
+	VOL("Vol", "#82BAEF");
 	
 	private final String nom;
+	private final String couleur;
 	
 	
 	// Constructeur
-	private TypeElement(String nom) {
+	private TypeElement(String nom, String couleur) {
 		this.nom = nom;
+		this.couleur = couleur;
 	}
 	
 	// Getter
@@ -33,5 +35,8 @@ public enum TypeElement {
         return nom;
     }
 	
+	public String getCouleur() {
+        return couleur;
+    }
 	
 }
