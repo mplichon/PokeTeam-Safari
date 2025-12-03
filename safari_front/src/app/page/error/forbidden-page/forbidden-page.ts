48c69cb13839083c95e09b00717417f5b36f8e4d
mplichon <plichon.matteo@gmail.com>
2025-12-03 21:30:11 +0100

    ajout de la page error 403

---
+++ safari_front/src/app/page/error/forbidden-page/forbidden-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forbidden-page',
+  imports: [],
+  templateUrl: './forbidden-page.html',
+  styleUrl: './forbidden-page.css',
+})
+export class ForbiddenPage {
+
+}
