f2f7833b5bedc9c55a011606fd4de1594ed46d3e
salom <salome.allali.kg@gmail.com>
2025-12-04 12:03:38 +0100

    deconnexion admin

--- safari_front/src/app/component/navbar-crud/navbar-crud.ts
+++ safari_front/src/app/component/navbar-crud/navbar-crud.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-crud',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-crud.css',
 })
 export class NavbarCrud {
+
+    constructor( 
+    private authService: AuthService, 
+    private router: Router){}
+
+
+    logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
